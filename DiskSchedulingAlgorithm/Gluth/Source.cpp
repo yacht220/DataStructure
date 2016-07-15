e96c4a432133a2671182f0d9d6b9855e85890562
Yizhou Zhou <yizhou.zhou@outlook.com>
2016-07-15 09:55:30 +0800

    Add Gluth's code.

---
+++ DiskSchedulingAlgorithm/Gluth/Source.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+const int largediskclinder = 1200;
+const int max = 2147483647;
+int diskinfo[largediskclinder];
+int dp[largediskclinder][largediskclinder];
+
+int disksectord(int x, int y)
+{
+    if (diskinfo[x] < diskinfo[y])
+    {
+        x = x ^ y;
+        y = x ^ y;
+        x = x ^ y;
+    }
+    int d = diskinfo[x] - diskinfo[y];
+    if (d > 180) d = 360 - d;
+    return d;
+}
+
+int min(int x, int y)
+{
+    return x > y ? y : x;
+}
+
+void compute(int num)
+{
+    dp[1][0] = disksectord(1, 0);
+    int i, j;
+    for (i = 2; i <= num; i++)
+    {
+        dp[i][i - 1] = max;
+        for (j = 0; j < i-1; j++)
+        {
+            dp[i][j] = dp[i - 1][j] + disksectord(i, i - 1);
+            dp[i][i - 1] = min(dp[i][i - 1], dp[i - 1][j] + disksectord(i, j));
+            
+        }
+    }
+}
+
+int main()
+{
+    ifstream myfile;
+    myfile.open("C:\\Users\\gaog6\\Desktop\\test\\test3.in");
+    int num;
+
+    
+    myfile >> num;
+    //cout << num << endl;
+    int x, y;
+    int largenum = 0;
+    int s = num;
+    diskinfo[0] = 0;
+    int i = 1;
+    while (num != 0)
+    {
+        myfile >> x >> y;
+        //cout << x << " " << y << endl;
+        diskinfo[i++] = y;
+        num--;
+    }
+    compute(s);
+    int re = max;
+    int simple = 0;
+    for (i = 0; i < s; i++)
+    {
+        re = min(re, dp[s][i] + disksectord(s, i));
+        simple += disksectord(i, i+1);
+    }
+
+    re += 800 * x + s * 10;
+    cout << re << endl;
+    simple += 400 * x + s * 10;
+    cout << simple << endl;
+    myfile.close();
+    system("pause");
+    return 0;
+}
